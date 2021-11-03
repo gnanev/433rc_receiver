64b74c7aec69b6e31da2ee914b64beb2c1a8ccec
Grigory Nanev <nanev1976@gmail.com>
2021-11-03 19:01:21 +0200

    initial

---
+++ 433rc_receiver.h
@@ -0,0 +1,11 @@
+﻿#ifndef _433RC_RECEIVER_H_
+#define _433RC_RECEIVER_H_
+
+#include "common.h"
+
+void setup433Receiver();
+BOOL get433Data(u32* data);
+
+BOOL getDiagnosticsBuffer(u08* buff);
+
+#endif 
